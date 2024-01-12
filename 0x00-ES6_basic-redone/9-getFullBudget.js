d330c189e83b00380ab80c9a1b53e846ee28e944
Mike15254 <mike8max10@gmail.com>
2024-01-12 03:23:09 +0300

    update: changed functions to arrow functions

---
+++ 0x00-ES6_basic-redone/9-getFullBudget.js
@@ -0,0 +1,32 @@
+import getBudgetObject from "./7-getBudgetObject";
+// export default function getFullBudgetObject(income, gdp, capita) {
+//     const budget = getBudgetObject(income, gdp, capita);
+
+//     const fullBudget = {
+//         ...budget,
+//         getIncomeInDollars(income) {
+//             return `$${income}`;
+//         },
+//         getIncomeInEuros(income) {
+//             return `${income} euros`;
+//         },
+//     };
+
+//     return fullBudget;
+// }
+
+const getFullBudgetObject = (income, gdp, capita) => {
+    const budget = getBudgetObject(income, gdp, capita);
+    const fullBudget = {
+        ...budget,
+        getIncomeInDollars(income) {
+            return (`$${income}`);
+        },
+        getIncomeInEuros(income) {
+            return (`${income}`);
+        }
+    };
+    return fullBudget
+}
+
+export default getFullBudgetObject;
