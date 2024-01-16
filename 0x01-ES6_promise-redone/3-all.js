4eeb5a5c6bf68b278661bde47ffc9a8efbace8e0
Mike15254 <mike8max10@gmail.com>
2024-01-16 03:12:14 +0300

    update: using Promise.all

---
+++ 0x01-ES6_promise-redone/3-all.js
@@ -0,0 +1,14 @@
+import {uploadPhoto, createUser} from "./utils";
+
+
+const handleProfileSignup=()=>{
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((results) =>{
+            console.log(results[0].body, results[1].firstName, results[1].l)
+        })
+        .catch(() =>{
+            console.log("Signup system offline")
+        })
+}
+
+export default handleProfileSignup;
