65d44e917bebb0a7562e37f953229f2c86ffee31
Mike15254 <mike8max10@gmail.com>
2024-01-19 05:22:39 +0300

    update: toString()

---
+++ 0x02-ES6_classes_redone/7-airport.js
@@ -0,0 +1,16 @@
+export default class Airport {
+    constructor(name, code) {
+        // if (typeof name !== 'string') {
+        //     throw TypeError("Name must be a String")
+        // }
+        // if (typeof code !== 'string') {
+        //     throw TypeError("Code must be a String")
+        // }
+        this._name = name;
+        this._code = code;
+    }
+
+    toString() {
+        return `[object ${this._code}]`
+    }
+}
