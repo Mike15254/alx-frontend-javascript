5035eed1a916e62afa5391f72f857cb052815125
Mike15254 <mike8max10@gmail.com>
2024-01-26 22:17:40 +0300

    update: filter and map correctly

--- 0x03-ES6_data_manipulation-redone/4-update_get_grade_by_city.js
+++ 0x03-ES6_data_manipulation-redone/4-update_get_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
     return students
-        .filter(student => student.location === city) // Filter students by city
+        .filter(student => student.location === city) // Filter students by cityku
         .map(student => {
             const gradeObj = newGrades.find(grade => grade.studentId === student.id);
             return {
                 ...student,
-                grade: gradeObj ? gradeObj.grade : 'N/A', // Update grade or set to 'N/A'
+                grade: gradeObj ? gradeObj.grade : 'N/A', // Update grade or set to 'N/A'c
             };
         });
 }  
