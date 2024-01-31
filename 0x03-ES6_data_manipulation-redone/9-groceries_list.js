8e4fc6cff256e092d2ad6b99f6043180bd98b660
Mike15254 <mike8max10@gmail.com>
2024-01-31 23:08:16 +0300

    update: mapped gloceries

--- 0x03-ES6_data_manipulation-redone/9-groceries_list.js
+++ 0x03-ES6_data_manipulation-redone/9-groceries_list.js
@@ -1,3 +1,10 @@
-export default function grocerieList() {
-    
-}
+export default function groceriesList() {
+    return new Map([
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5],
+    ]);
+  }
+  
