75f4f68ffd205144ae68f1f394ff53168ded8a5b
Mike15254 <mike8max10@gmail.com>
2024-02-10 21:11:13 +0300

    update: corrected function

--- 0x03-ES6_data_manipulation-redone/10-update_uniq_item.js
+++ 0x03-ES6_data_manipulation-redone/10-update_uniq_item.js
@@ -1,3 +1,14 @@
-const updateUniqueItems = () => {
-
-}
+export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+      throw Error('Cannot process');
+    }
+  
+    map.forEach((el, key) => {
+      if (el === 1) {
+        map.set(key, 100);
+      }
+    });
+  
+    return map;
+  }
+  
